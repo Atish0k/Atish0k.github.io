793ae2fd594677806fa86dfa41d5d24cd62d3a04
tishok <ma-l.lezhenko@yandex.ru>
2022-10-28 13:21:15 +0300

    проверка

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+
+    <title>Новостное агенство</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+
+</head>
+<body>
+<div id="black-bar">
+    <div class="fixed-container">
+        Самые <a href="3#">свежие новости</a> в реальном времени
+        <div id="date-time">11:30 11/10/20</div>
+    </div>
+</div>
+</body>
+</html>
