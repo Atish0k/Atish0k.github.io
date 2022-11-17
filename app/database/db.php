2d8201a224704835e250fcaa66e87af01f9c7f67
Arti-pc <ma-l.lezhenko@yandex.ru>
2022-11-18 00:58:46 +0300

    add functions, check error, selectall/selectone, insert, update, delete

--- app/database/db.php
+++ app/database/db.php
@@ -7,21 +7,129 @@ function tt($value){
     echo '</pre>';
 }
 
-function selectAll($table){
-    global $pdo;
-
-    $sql = "SELECT * FROM $table";
-    $query = $pdo->prepare($sql);
-    $query->execute();
+//Проверка выполнения запроса к БД
+function dbCheckError($query){
     $errInfo = $query -> errorInfo();
-
     if($errInfo[0] !== PDO::ERR_NONE){
         echo $errInfo[2];
         exit();
     }
+    return true;
+}
 
+//Запрос на получение данных с одной таблицы
+function selectAll($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        echo 'Потихонку работаем';
+        foreach ($params as $key => $value){
+            if(!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if($i === 0){
+                $sql = $sql . " WHERE $key=$value";
+            }else{
+                $sql = $sql . " AND $key=$value";
+            }
+            $i++;
+        }
+    }
+
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
     return $query -> fetchAll();
 }
-tt(selectAll('users'));
+
+//Запрос на получение одной строки с выбранной таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+    $sql = "SELECT * FROM $table";
+
+    if(!empty($params)){
+        $i = 0;
+        echo 'Потихонку работаем';
+        foreach ($params as $key => $value){
+            if(!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if($i === 0){
+                $sql = $sql . " WHERE $key=$value";
+            }else{
+                $sql = $sql . " AND $key=$value";
+            }
+            $i++;
+        }
+    }
+
+    //$sql = $sql . " LIMIT 1";
+    $query = $pdo->prepare($sql);
+    $query->execute();
+    dbCheckError($query);
+    return $query -> fetch();
+}
+//Запись в таблицу БД
+function insert($table, $params){
+    global $pdo;
+    $i = 0;
+    $coll = '';
+    $mask = '';
+    foreach($params as $key => $value){
+        if($i === 0){
+            $coll = $coll ."$key";
+            $mask = $mask ."'"."$value"."'";
+        }else {
+            $coll = $coll . ", $key";
+            $mask = $mask . ", '" . "$value" . "'";
+        }
+        $i++;
+    }
+
+    $sql = "INSERT INTO $table ($coll) VALUES ($mask)";
+
+//    tt($sql);
+//    exit();
+    $query = $pdo->prepare($sql);
+    $query->execute($params = []);
+    dbCheckError($query);
+}
+
+
+//Обновление данных
+function update($table, $id, $params){
+    global $pdo;
+    $i = 0;
+    $str = '';
+    foreach($params as $key => $value){
+        if($i === 0){
+            $str = $str . $key . "= '".$value."'";
+        }else {
+            $str = $str .", ".$key . "= '".$value."'";
+        }
+        $i++;
+    }
+    //UPDATE users SET username = test WHERE id = 16
+    $sql = "UPDATE $table SET $str WHERE id = $id";
+
+//    tt($sql);
+//    exit();
+    $query = $pdo->prepare($sql);
+    $query->execute($params = []);
+    dbCheckError($query);
+}
+
+//Удаление данных
+function delete($table, $id){
+    global $pdo;
+    $sql = "DELETE FROM $table WHERE id = $id";
+    $query = $pdo->prepare($sql);
+    $query->execute($params = []);
+    dbCheckError($query);
+}
+delete('users',8);
+
 
 
