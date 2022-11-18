0602c16c84a3d8d0654490602269d0fba2f64d20
Arti-pc <ma-l.lezhenko@yandex.ru>
2022-11-18 10:11:08 +0300

    10.03

--- app/controllers/users.php
+++ app/controllers/users.php
@@ -1,5 +1,5 @@
 <?php
-//include "app/database/db.php";
+include "app/database/db.php";
 
 $errMsg = '';
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
-//include ("app/controllers/users.php");
-include ("app/controllers/user.php");
+include ("app/controllers/users.php");
+
 
 ?>
 
